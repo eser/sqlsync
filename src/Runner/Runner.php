adcf5b7711b1ea5e5350b01935148a3075c993fe
Eser Ozvataf <eser@ozvataf.com>
2015-09-28 18:17:41 +0300

    * error handling.

--- src/Runner/Runner.php
+++ src/Runner/Runner.php
@@ -3,6 +3,7 @@
 namespace SqlSync\Runner;
 
 use Scabbia\Services\Services;
+use ErrorException;
 use mysqli;
 use MySQLDump;
 
@@ -29,27 +30,50 @@ class Runner
     {
         $tDatabase = rtrim($this->request->get("db", ""));
 
-        if (strlen($tDatabase) === 0) {
-            $this->error("Database is not specified.");
+        if (!$this->establishDbConnection($tDatabase)) {
             return;
         }
-
-        $this->db = new mysqli(
-            $this->config["database"]["host"],
-            $this->config["database"]["username"],
-            $this->config["database"]["password"],
-            $tDatabase
-        );
 
         $tDump = new MySQLDump($this->db);
         $tDump->write();
     }
 
+    protected function establishDbConnection($uDatabase)
+    {
+        if (strlen($uDatabase) === 0) {
+            $this->error("Database is not specified.");
+            return false;
+        }
+
+        try {
+            $this->db = new mysqli(
+                $this->config["database"]["host"],
+                $this->config["database"]["username"],
+                $this->config["database"]["password"],
+                $uDatabase
+            );
+
+            if (isset($this->config["database"]["commands"])) {
+                foreach ((array)$this->config["database"]["commands"] as $tCommand) {
+                    $this->db->query($tCommand);
+                }
+            }
+        } catch (ErrorException $ex) {
+            $this->error(sprintf("Database connection error: '%s'.", $ex->getMessage()));
+            return false;
+        }
+
+        return true;
+    }
+
     protected function error($uMessage)
     {
-        // TODO: Return error as HTTP status (403?)
+        // TODO: Return HTTP status 403 within Response class.
+        header("HTTP/1.0 403 Forbidden");
+
         $this->formatter->writeHeader(2, "Error");
         $this->formatter->write($uMessage);
+
         $this->exitCode = 1;
     }
 }
