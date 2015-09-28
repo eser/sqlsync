4d8d82708945895a7f8ca006d658b1067c3593a3
Eser Ozvataf <eser@ozvataf.com>
2015-09-28 17:53:02 +0300

    * updated sample config.

--- config.sample.php
+++ config.sample.php
@@ -1,3 +1,9 @@
-<?php return array(
-    "logpath" => "sqlsync.log"
-);
+<?php return [
+    "logpath" => "sqlsync.log",
+    "database" => [
+        "host"     => "localhost",
+        "username" => "root",
+        "password" => "",
+        "commands" => "SET NAMES 'utf8'"
+    ]
+];
