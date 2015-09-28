f82627ae6c186400642ba2db7e87a0e9f1cd984e
Eser Ozvataf <eser@ozvataf.com>
2015-09-28 17:52:32 +0300

    * added dump code.

--- src/Runner/Runner.php
+++ src/Runner/Runner.php
@@ -3,18 +3,53 @@
 namespace SqlSync\Runner;
 
 use Scabbia\Services\Services;
+use mysqli;
+use MySQLDump;
 
 class Runner
 {
     public $exitCode = 0;
-    public $services;
 
-    public function __construct(Services $services)
+    protected $services;
+    protected $config;
+    protected $request;
+    protected $formatter;
+
+    protected $db = null;
+
+    public function __construct(Services $uServices)
     {
-        $this->services = $services;
+        $this->services = $uServices;
+        $this->config = $this->services["config"];
+        $this->request = $this->services["request"];
+        $this->formatter = $this->services["formatter"];
     }
 
     public function start()
     {
+        $tDatabase = rtrim($this->request->get("db", ""));
+
+        if (strlen($tDatabase) === 0) {
+            $this->error("Database is not specified.");
+            return;
+        }
+
+        $this->db = new mysqli(
+            $this->config["database"]["host"],
+            $this->config["database"]["username"],
+            $this->config["database"]["password"],
+            $tDatabase
+        );
+
+        $tDump = new MySQLDump($this->db);
+        $tDump->write();
+    }
+
+    protected function error($uMessage)
+    {
+        // TODO: Return error as HTTP status (403?)
+        $this->formatter->writeHeader(2, "Error");
+        $this->formatter->write($uMessage);
+        $this->exitCode = 1;
     }
 }
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -37,10 +37,18 @@ if (isset($tConfig["logpath"]) && $tConfig["logpath"] !== null) {
     );
 }
 
+// request
+$tRequest = \Scabbia\LightStack\Request::generateFromGlobals();
+
+// formatter
+$tFormatter = \Scabbia\Formatters\Formatters::getCurrent();
+
 // services
 $tServices = new \Scabbia\Services\Services();
 $tServices["classLoader"] = $tClassLoader;
 $tServices["config"] = $tConfig;
 $tServices["log"] = $tLog;
+$tServices["request"] = $tRequest;
+$tServices["formatter"] = $tFormatter;
 
 return $tServices;
